83fd437fe50ab288ff2b7f1b0664182a4fb95b48
francis-100 <mayiekaf@gmail.com>
2024-11-26 10:05:02 +0300

    sockets completed

---
+++ SimpleServer.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+public class SimpleServer {
+    public static void main(String[] args) {
+        int port = 65432;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+                String text;
+                while ((text = reader.readLine()) != null) {
+                    System.out.println("Received: " + text);
+                    writer.println("Echo: " + text);
+                }
+                socket.close();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
